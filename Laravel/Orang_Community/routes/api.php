b6de302a4d5f5aab0ba0520833d27287f809eee5
Zaid-ayoub <zaidayyoubb@gmail.com>
2024-12-05 09:33:50 +0300

    "Update the api routes"

--- Laravel/Orang_Community/routes/api.php
+++ Laravel/Orang_Community/routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\ActivityController as ControllersActivityController;
-=======
 use App\Http\Controllers\Api\ApiController;
->>>>>>> 5043e3e10bcfeef6ea1e6855dcd0e7a7ae71f3b5
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\landingPage\PostController;
@@ -24,16 +21,9 @@ use App\Http\Controllers\ActController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-<<<<<<< HEAD
-Route::get("index", [PostController::class, 'index']);
-Route::post("posts/share", [PostController::class, 'share']);
-Route::get("activities", [ActController::class, 'getActivities']);
-Route::group([
-    "middleware" => ["auth:sanctum"]
-], function(){
-    
-});
-=======
+
+
+
 
 Route::post('register' , [ApiController::class , 'register']);
 
@@ -43,8 +33,8 @@ Route::group([
     'middleware' => ["auth:sanctum"]
 ], function(){
     Route::get('profile' , [ApiController::class , 'profile']);
-
+    Route::get("index", [PostController::class, 'index']);
+    Route::post("posts/share", [PostController::class, 'share']);
+    Route::get("activities", [ActController::class, 'getActivities']);
     Route::get('logout' , [ApiController::class , 'logout']);
 });
-
->>>>>>> 5043e3e10bcfeef6ea1e6855dcd0e7a7ae71f3b5
